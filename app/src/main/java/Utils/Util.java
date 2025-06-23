90718923b77f4e4fc2cdf0093563baeeb62f05d4
Arhizmei <arhizmei@gmail.com>
2025-06-23 13:10:16 +0300

    lesson 126

--- app/src/main/java/Utils/Util.java
+++ app/src/main/java/Utils/Util.java
@@ -3,6 +3,8 @@ package Utils;
 public class Util {
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "carsDB";
+    private static final String TABLE_NAME = "cars";
+
     private static final String KEY_ID = "id";
     private static final String KEY_NAME = "name";
     private static final String KEY_PRICE = "price";
